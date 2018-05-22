e388f6def2c46799b257fb473127d26d3f282270
Winston Spencer Sonnesen <winston.sonnesen@totvs.com.br>
2018-05-22 13:54:32 -0300

    Reestruturação do projeto

--- tccproject/courses/admin.py
+++ tccproject/courses/admin.py
@@ -1,22 +1,13 @@
+from courses.models import Course
 from django.contrib import admin
 
-from courses.models import Course
-from units.models import Unit
 
-
-class UnitInlineModelAdmin(admin.TabularInline):
-    model = Unit
-    min_num = 1
-    extra = 0
-    
-    
 class CourseModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'created_at', 'category', 'instructor', 'keyword_list')
     autocomplete_fields = ('category', 'instructor')
     readonly_fields = ('created_at',)
     list_filter = ('title', 'category', 'instructor')
     search_fields = ('title', 'category', 'instructor')
-    inlines = [UnitInlineModelAdmin]
     
     def get_queryset(self, request):
         return super(
--- tccproject/dashboard/views.py
+++ tccproject/dashboard/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 class DashboardView(View):
     template_name = 'index.html'
-    context = {'title': 'Dashboard'}
+    context = {'title': 'My Dashboard'}
     
     def get(self, request, *args, **kargs):
         return render(request, self.template_name, self.context)
