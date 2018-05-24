9c2ab9c8eec8955100c3b5f519baf1afc97e1fc4
Winston Spencer Sonnesen <sonnesen@gmail.com>
2018-05-24 16:19:01 -0300

    Atividades da Unidade

--- tccproject/courses/admin.py
+++ tccproject/courses/admin.py
@@ -1,13 +1,23 @@
-from courses.models import Course
 from django.contrib import admin
 
+from courses.models import Course
+from units.models import Unit
 
+
+class UnitInline(admin.TabularInline):
+    model = Unit
+    extra = 1
+    verbose_name = 'Course Unit'
+    verbose_name_plural = 'Course Units'
+    
+    
 class CourseModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'created_at', 'category', 'instructor', 'keyword_list', 'description', 'image')
     autocomplete_fields = ('category', 'instructor')
     readonly_fields = ('created_at',)
     list_filter = ('title', 'category', 'instructor')
     search_fields = ('title', 'category', 'instructor')
+    inlines = [UnitInline, ]
     
     def get_queryset(self, request):
         return super(
--- tccproject/units/admin.py
+++ tccproject/units/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+
+from activities.models import Activity
 from units.models import Unit
+
+
+class ActivityInline(admin.TabularInline):
+    model = Activity
+    extra = 1
+    verbose_name = 'Unit Activity'
+    verbose_name_plural = 'Unit Activities'
+    
 
 class UnitModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'course')
     autocomplete_fields = ('course',)
     search_fields = ('title',)
     list_filter = ('course',)
+    inlines = [ActivityInline, ]
+
     
 admin.site.register(Unit, UnitModelAdmin)
