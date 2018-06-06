3d68870874815ddfbbc1d529fc98cd0ec4e071c1
Winston Spencer Sonnesen <sonnesen@gmail.com>
2018-06-05 23:58:04 -0300

    Paginação da api de listagem de instrutores

--- instructors/api/viewsets.py
+++ instructors/api/viewsets.py
@@ -1,10 +1,12 @@
 from django.db.models.functions.base import Lower
 from django.db.models.query_utils import Q
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from instructors.api.serializers import InstructorSerializer
 from instructors.models import Instructor
-from django.core.paginator import Paginator
+from _collections import OrderedDict
 
 
 class InstructorViewSet(ModelViewSet):
@@ -13,21 +15,14 @@ class InstructorViewSet(ModelViewSet):
 #     permission_classes = (IsAuthenticated,)
 #     authentication_classes = (TokenAuthentication,)
 
-    draw_page = 0
-    register_per_page = 0
-    first_record_in_page = 0
-    page = 0
     error = ''
     recordTotal = 0
     recordsFiltered = 0
     
     def get_queryset(self):
-        self.draw_page = int(self.request.query_params.get('draw'))
-        self.registers_per_page = int(self.request.query_params.get('length'))
-        self.first_record_in_page = int(self.request.query_params.get('start'))
-        self.page = int(self.first_record_in_page / self.registers_per_page) + 1
-        self.error = ''
- 
+        # requirido pelo datatables.js
+        self.draw_page = int(self.request.query_params.get('draw', 0))
+        
         queryset = Instructor.objects.all()
         self.recordsTotal = queryset.count()
         self.recordsFiltered = self.recordsTotal
@@ -35,15 +30,16 @@ class InstructorViewSet(ModelViewSet):
         search_value = self.request.query_params.get('search[value]')
         if search_value:
             queryset = queryset.filter(
-                Q(name__icontains=search_value) |
+                Q(name__icontains=search_value) | 
                 Q(contact__icontains=search_value)
             )
             self.recordsFiltered = queryset.count()
  
-        if self.request.query_params.get('order[0][column]') == '1':
-            query_order_by = 'contact'
-        elif self.request.query_params.get('order[0][column]') == '2':
-            query_order_by = 'about'
+        if self.request.query_params.get('order[0][column]'):
+            query_order_by = self.request.query_params.get(
+                'columns[{}][data]'.format(
+                    self.request.query_params.get('order[0][column]'))
+                )
         else:
             query_order_by = 'name'
     
@@ -51,31 +47,38 @@ class InstructorViewSet(ModelViewSet):
             queryset = queryset.order_by(Lower(query_order_by).desc())
         else:
             queryset = queryset.order_by(Lower(query_order_by))
- 
-#             paginator = Paginator(queryset, registers_per_page)
-#             instructors_page = paginator.get_page(page)
-#             instructors_list = list(instructors_page.object_list.values())
-#  
-#             json = {
-#                 'draw': draw_page,
-#                 'recordsTotal': recordsTotal,
-#                 'recordsFiltered': recordsFiltered,
-#                 'data': instructors_list,
-#                 'error': error
-#             }
-#  
-#             return JsonResponse(json)
 
         return queryset
         
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        self.paginator = Paginator(queryset, self.registers_per_page)
+        if request.query_params.get('length'):
+            # listagem utilizando paginação do datatables.js
+            registers_per_page = int(request.query_params.get('length'))
+            first_record_in_page = int(request.query_params.get('start', 0))
+            request.query_params._mutable = True       
+            request.query_params['page'] = int(
+                first_record_in_page / registers_per_page) + 1
+            request.query_params._mutable = False
         
-        #instructors_page = paginator.get_page(self.page)
-        #instructors_list = list(instructors_page.object_list.values())
+        self.pagination_class = PageNumberPagination             
+        queryset = self.filter_queryset(self.get_queryset())
+        paginator = PageNumberPagination()
+        paginator.page_size_query_param = 'length'
+        instructors_page = paginator.paginate_queryset(queryset, request)
         
-        return super().list(request, *args, **kwargs) 
+        if instructors_page is not None:
+            serializer = self.get_serializer(instructors_page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
     
     def get_paginated_response(self, data):
-        return ModelViewSet.get_paginated_response(self, data)
+        return Response(OrderedDict([
+                ('draw', self.draw_page),
+                ('recordsTotal', self.recordsTotal),
+                ('recordsFiltered', self.recordsFiltered),
+                ('data', data),
+                ('error', self.error)                
+            ]))
+    
