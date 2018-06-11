651eb8fd467b987781c87bfafcef5e6904ed12ad
Winston Spencer Sonnesen <sonnesen@gmail.com>
2018-06-10 23:05:50 -0300

    Serialização das tags na api de cursos

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,34 +1,38 @@
-from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from categories.models import Category
 from courses.models import Course
 from instructors.models import Instructor
+from categories.api.serializers import CategorySerializer
+from instructors.api.serializers import InstructorSerializer
+from taggit_serializer.serializers import TagListSerializerField,\
+    TaggitSerializer
 
 
-class CourseSerializer(ModelSerializer):
-    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
-    instructor = serializers.PrimaryKeyRelatedField(queryset=Instructor.objects.all())
-    
+class CourseSerializer(TaggitSerializer, ModelSerializer):
+    category = CategorySerializer()
+    instructor = InstructorSerializer()
+    keywords = TagListSerializerField()
+
     class Meta:
         model = Course
         fields = ('__all__')
         read_only_fields = ('created_at',)
-        
+
     def create(self, validated_data):
         category_data = validated_data.pop('category', None)
         instructor_data = validated_data.pop('instructor', None)
-        
+
         if category_data:
             category, created = Category.objects.get_or_create(**category_data)
-        
+
         if instructor_data:
             instructor, created = Instructor.objects.get_or_create(**instructor_data)
-                
+
         course = Course.objects.create(**validated_data)
         course.category = category
         course.instructor = instructor
         course.save()
-        
+
         return course
-        
+
--- tccproject/settings.py
+++ tccproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_swagger',
     'taggit',
+    'taggit_serializer',
     'dashboard',
     'instructors',
     'categories',
