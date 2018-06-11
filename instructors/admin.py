cd89fcafc0ea5c27947cc5a6aa582a075e4691ce
Winston Spencer Sonnesen <sonnesen@gmail.com>
2018-06-10 23:15:46 -0300

    Pequeno ajuste no registro da classe

--- instructors/admin.py
+++ instructors/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from instructors.models import Instructor
 
 
+@admin.register(Instructor)
 class InstructorModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'contact', 'about')
     search_fields = ('name',)
 
 
-admin.site.register(Instructor, InstructorModelAdmin)
